9ba6769405bb661c56e594ae8f3e4f71b57e9b85
TimoshRR <109026289+TimoshRR@users.noreply.github.com>
2022-08-12 20:53:54 +0530

    Working with stack

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import operator;
+import operator
 class Evaluate:
   """This class validates and evaluate postfix expression.
   Attributes:
@@ -9,7 +9,7 @@ class Evaluate:
     # Write your code here
 
 
-  def __init__(self, size):
+  def _init_(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
       size_of_stack: An integer to set the size of stack.
@@ -26,8 +26,10 @@ class Evaluate:
       True if it is empty, else returns False.
     """
       # Write your code here
-      if self.top == -1:
+    if len(self.stack)==0:
         return True
+    else:
+        return False
 
 
   def _pop(self):
@@ -37,9 +39,10 @@ class Evaluate:
       The data which is popped out if the stack is not empty.
     """
     # Write your code here
-    self.stack.pop()
-    self.top-=1
-    
+    if self.isEmpty():
+      print("Your stack is empty")
+    else:
+        return self.stack.pop()
 
 
   def push(self, operand):
@@ -50,9 +53,12 @@ class Evaluate:
     """
     # Write your code here
     self.operand = operand
-    if self.top == self.size_of_stack - 1:
-      self.stack.append(self.operand)
-      self.top+=1
+    if len(self.stack)==self.size_of_stack:
+      print("Your stack is full")
+    else:
+        self.stack.append(self.operand)
+        self.top+=1
+       
 
 
   def validate_postfix_expression(self, expression):
@@ -63,19 +69,15 @@ class Evaluate:
     Returns:
       True if the expression is valid, else returns False.
     """
-    # Write your code here
-    self.expression = expression
-     for i in self.expression:
-      if x=="+" or x=="-" or x== "/" or x=="*" or x=="%" or x=="**":
-        value1 = self._pop()
-        value2 = self._pop()
-        dict = {"+" : operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv, "%": operator.mod, "**": operator.pow}
-        self.push(dict[x](value2,value1))
-      else:
-        self.push(int(x))
-        
-        
-    
+    for x in expression:
+        if x.isnumeric()==True:
+            return True
+        else:
+            if x=="+" or x=="-" or x=="" or x=="/" or x=="*":
+                return True
+            else:
+                return False
+   
 
 
   def evaluate_postfix_expression(self, expression):
@@ -86,16 +88,16 @@ class Evaluate:
     Returns:
       The result of evaluated postfix expression.
     """
-    # Write your code here
-    for i in self.expression:
-      if x=="+" or x=="-" or x== "/" or x=="*" or x=="%" or x=="**":
-        value1 = self._pop()
-        value2 = self._pop()
-        dict = {"+" : operator.add, "-": operator.sub, "*": operator.mul, "/": operator.floordiv, "%": operator.mod, "**": operator.pow}
-        self.push(dict[x](value2,value1))
-      else:
-        self.push(int(x))
-    return self.stack[0]        
+    for i in expression:
+        if i.isnumeric():
+            self.push(int(i))
+        else:
+           a=self._pop()
+           b=self._pop()
+           operations_dictionary={"+":operator.add,"-":operator.sub,"*":operator.mul,"/":operator.floordiv,"^":operator.pow}
+           self.push(operations_dictionary[i](b,a))
+     
+    return self.stack[0]
 
 
 # Do not change the following code
